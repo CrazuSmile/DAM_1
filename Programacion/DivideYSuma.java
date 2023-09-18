2066fb712812f49eb728bf21deada8271f992dce
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-18 10:44:43 +0000

    new file:   Programacion/DivideYSuma.java

---
+++ Programacion/DivideYSuma.java
@@ -0,0 +1,10 @@
+/*Un programa que calcula una division y una suma */
+
+public class DivideYSuma {
+    public static void main(String[] args) {
+        double dividend=  20.0;
+        double divisor= 6.0;
+        double sumarAlFinal= 3.0;
+        System.out.println((dividend/divisor)+sumarAlFinal);
+    } 
+}
