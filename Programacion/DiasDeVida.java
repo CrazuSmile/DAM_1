1038628cf8c55136c26bfcb6e7cbcf8141d82b84
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-18 10:40:56 +0000

    new file:   Programacion/DiasDeVida.java

---
+++ Programacion/DiasDeVida.java
@@ -0,0 +1,12 @@
+/*
+ * Dias de vida
+ */
+
+public class DiasDeVida {
+
+    public static void main(String[] args) {
+        int edad=23;
+        int dias=edad*360;
+        System.out.println("No dire a nadie que tienes "+dias+" dias");
+    }
+}
