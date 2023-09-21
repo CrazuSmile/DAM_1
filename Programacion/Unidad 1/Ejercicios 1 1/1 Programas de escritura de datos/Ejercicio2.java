91d00df6330ea925f38ca8ed56f97bbafd4bb5fa
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-21 07:50:24 +0000

    modified:   Programacion/Unidad 1/Ejercicios 1 1/1 Programas de escritura de datos/Ejercicio2.java

--- Programacion/Unidad 1/Ejercicios 1 1/1 Programas de escritura de datos/Ejercicio2.java
+++ Programacion/Unidad 1/Ejercicios 1 1/1 Programas de escritura de datos/Ejercicio2.java
@@ -4,6 +4,7 @@ public class Ejercicio2 {
 
     public static void main(String[] args) {
         String nombre1, nombre2, apellido1, apellido2;
+        int diaNa, mesNa, añoNa;
         Scanner lector = new Scanner(System.in);
         
         System.out.print("Escribe tu nombre: ");
@@ -18,8 +19,14 @@ public class Ejercicio2 {
 
         System.out.print("Segundo apellido: ");
         apellido2 = lector.next();
+        lector.nextLine();
 
-        System.out.println("Tu nombre completo es "+nombre1+" "+nombre2+" "+apellido1+" "+apellido2+" ");
+        System.out.println("Fecha de naciminto");
+        diaNa = lector.nextInt();
+        mesNa = lector.nextInt();
+        añoNa = lector.nextInt();
+
+        System.out.println("Tu nombre completo es "+nombre1+" "+nombre2+" "+apellido1+" "+apellido2+"\nFecha De Naciemiento: "+diaNa+"/"+mesNa+"/"+añoNa);
         
 
     }
