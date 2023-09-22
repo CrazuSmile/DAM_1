9851475c6759e7429c4c41522eb63380cdcf03ce
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-22 10:56:30 +0000

    modified:   Programacion/Unidad 1/Teoria/Ejercicios/Suma.java

--- Programacion/Unidad 1/Teoria/Ejercicios/Suma.java
+++ Programacion/Unidad 1/Teoria/Ejercicios/Suma.java
@@ -3,24 +3,22 @@ import java.util.Scanner;
 public class Suma {
 
     public static void main(String[] args) {
-        int valor1, valor2, resultado;    
+        double valor1, valor2;    
         Scanner lector = new Scanner(System.in);
 
         System.out.print("Introduzaca un numero: ");
         
-        valor1 = lector.nextInt();
+        valor1 = lector.nextDouble();
 
         lector.nextLine();
 
         System.out.print("Introduzca un segundo numero: ");
 
-        valor2 = lector.nextInt();
+        valor2 = lector.nextDouble();
 
         lector.nextLine();
 
-        resultado = valor1 + valor2;
-
-        System.out.println("La suma es: "+valor1+" + "+valor2+" = "+resultado );
+        System.out.println("La suma es: "+valor1+" + "+valor2+" = "+(valor1+valor2) );
 
 
     }
