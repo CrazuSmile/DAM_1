e3a344c8fd1537ec51c40379fd789055ae11d51e
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-25 08:06:58 +0000

    modified:   Programacion/Unidad 1/Ejercicios 1 1/1 Programas de escritura de datos/Ejercicio2.java 	new file:   "Programacion/Unidad 1/Ejercicios 1 1/3 Variables y expresiones aritm\303\251ticas/Ejercicio6.java"

---
+++ Programacion/Unidad 1/Ejercicios 1 1/3 Variables y expresiones aritméticas/Ejercicio6.java
@@ -0,0 +1,25 @@
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        double resultado, interes, haceinda, interesHacienda, ingreso;
+
+        ingreso = 2000;
+
+        interes = ((ingreso * 2.75) / 100) / 2;
+
+        System.out.println("Primero se calcula el interes: " + interes);
+
+        haceinda = (interes * 18) / 100;
+
+        System.out.println("Ahora, se calcula el 18% de hacienda: " + haceinda);
+
+        interesHacienda = interes - haceinda;
+
+        System.out.println("Se calcula el total de intereses: " + interesHacienda);
+
+        resultado = interesHacienda + ingreso;
+
+        System.out.println("Por ultimo se suma lo que el banco te pagara: " + resultado + "€");
+    }
+
+}
