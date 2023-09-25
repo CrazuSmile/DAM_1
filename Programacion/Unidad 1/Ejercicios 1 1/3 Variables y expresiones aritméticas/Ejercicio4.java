1bc4eda5b6e3d84e25370149a6e10df7e877b1cb
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-25 06:31:57 +0000

    new file:   "Programacion/Unidad 1/Ejercicios 1 1/3 Variables y expresiones aritm\303\251ticas/Ejercicio4.java"

---
+++ Programacion/Unidad 1/Ejercicios 1 1/3 Variables y expresiones aritméticas/Ejercicio4.java
@@ -0,0 +1,18 @@
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        double resultadoA, resultadoB, resultadoC, resultadoD;
+
+        resultadoA = 25 + 20 - 15;
+
+        resultadoB = 20 * 10 + 15 * 10;
+
+        resultadoC = 20 * 10 / 2 - 20 / 5 * 3;
+
+        resultadoD = 15 / 10 * 2 + 3 / 4 * 8;
+
+        System.out.print("A: " + resultadoA + "\nB: " + resultadoB + "\nC: " + resultadoC + "\nD: " + resultadoD);
+
+    }
+
+}
