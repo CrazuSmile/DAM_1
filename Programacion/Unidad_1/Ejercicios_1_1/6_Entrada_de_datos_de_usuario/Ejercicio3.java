f5c1cf589a597bfb510e3da53d50b775fb806081
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-09-28 06:45:39 +0000

    modified:   Programacion/Unidad_1/Ejercicios_1_1/6_Entrada_de_datos_de_usuario/Ejercicio3.java

--- Programacion/Unidad_1/Ejercicios_1_1/6_Entrada_de_datos_de_usuario/Ejercicio3.java
+++ Programacion/Unidad_1/Ejercicios_1_1/6_Entrada_de_datos_de_usuario/Ejercicio3.java
@@ -1,8 +1,35 @@
 import java.util.Scanner;
 
-
 public class Ejercicio3 {
     public static void main(String[] args) {
-        double 
+        float precioLitro1, pagado1, km1;
+        float precioLitro2, pagado2;
+        float km3;
+        float litros1, litros2, consumo100, precioKM;
+
+        Scanner tcl = new Scanner(System.in);
+
+        System.out.println("Introduce precio/litro, la cantidad pagada y los kilometros realizado: ");
+        precioLitro1 = tcl.nextFloat();
+        pagado1 = tcl.nextFloat();
+        km1 = tcl.nextFloat();
+
+        System.out.println("Introduce precio/litro, la cantidad pagada: ");
+        precioLitro2 = tcl.nextFloat();
+        pagado2 = tcl.nextFloat();
+
+        System.out.println("Introduce los kilometros: ");
+        km3 = tcl.nextFloat();
+
+        litros1 = pagado1 / precioLitro1;
+        litros2 = pagado2 / precioLitro2;
+
+        consumo100 = 100 * (litros1 + litros2) / (km1 + km3);
+
+        precioKM = (pagado1 + pagado2) / (km1 + km3);
+
+        System.out.println("Consumo por 100km: " + consumo100);
+        System.out.println("Precio KM: " + precioKM);
+
     }
 }
