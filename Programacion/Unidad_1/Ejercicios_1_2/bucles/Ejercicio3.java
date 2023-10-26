355ab7fe49c4f4e4448fa98536cdff3e70c34acc
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-10-26 08:07:25 +0000

    modified:   Programacion/Unidad_1/Ejercicios_1_2/bucles/Ejercicio3.java 	modified:   Programacion/Unidad_1/Ejercicios_1_2/bucles/Ejercicio9.java

--- Programacion/Unidad_1/Ejercicios_1_2/bucles/Ejercicio3.java
+++ Programacion/Unidad_1/Ejercicios_1_2/bucles/Ejercicio3.java
@@ -2,21 +2,27 @@ package Programacion.Unidad_1.Ejercicios_1_2.bucles;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        int impar, par, multiCinco;
+        int impar, par, multiCinco, count = 0;
 
         for (impar = 1; impar <= 99; impar = impar + 2) {
             System.out.print(impar + ", ");
+            count++;
         }
+        System.out.println("Hay " + count + " impares");
         System.out.println();
-
+        count = 1;
         for (par = 2; par <= 100; par += 2) {
             System.out.print(par + ", ");
+            count++;
         }
+        System.out.println("Hay " + count + " pares");
         System.out.println();
 
+        count = 1;
         for (multiCinco = 5; multiCinco <= 100; multiCinco += 5) {
             System.out.print(multiCinco + ", ");
-
+            count++;
         }
+        System.out.println("Hay "+count+" multiplos de 5");
     }
 }
--- Programacion/Unidad_1/Ejercicios_1_2/bucles/Ejercicio9.java
+++ Programacion/Unidad_1/Ejercicios_1_2/bucles/Ejercicio9.java
@@ -1,8 +1,46 @@
 package Programacion.Unidad_1.Ejercicios_1_2.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio9 {
     public static void main(String[] args) {
-        int numero;
-        
+        int numero, contador = 2;
+        boolean primo = true;
+
+        Scanner lector = new Scanner(System.in);
+
+        System.out.print("Introduce un numero a factorizar: ");
+        numero = lector.nextInt();
+
+        while ((primo) && (contador != numero)) {
+            if (numero % contador == 0)
+                primo = false;
+            contador++;
+        }
+        if (primo) {
+            System.out.println(numero);
+
+        } else {
+            while (numero > 1) {
+                if (numero % 2 == 0) {
+                    System.out.print("*2*");
+                    numero /= 2;
+                }
+                if (numero % 3 == 0) {
+                    System.out.print("*3*");
+                    numero /= 3;
+                }
+                if (numero % 5 == 0) {
+                    System.out.print("*5*");
+                    numero /= 5;
+                }
+                if (numero % 7 == 0) {
+                    System.out.print("*7*");
+                    numero /= 7;
+                }
+
+            }
+
+        }
     }
 }
