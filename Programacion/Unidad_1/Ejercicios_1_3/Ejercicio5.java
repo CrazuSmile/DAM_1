0600b6be50bfd279904d468f344a01a1e505d862
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-11-02 08:52:12 +0000

    modified:   Programacion/Unidad_1/Ejercicios_1_3/Ejercicio4.java 	new file:   Programacion/Unidad_1/Ejercicios_1_3/Ejercicio5.java 	new file:   Programacion/Unidad_1/Ejercicios_1_3/Ejercicio6.java

--- Programacion/Unidad_1/Ejercicios_1_3/Ejercicio5.java
+++ Programacion/Unidad_1/Ejercicios_1_3/Ejercicio5.java
@@ -2,7 +2,7 @@ package Programacion.Unidad_1.Ejercicios_1_3;
 
 import java.util.Scanner;
 
-public class Ejercicio4 {
+public class Ejercicio5 {
     static int mayor(int a, int b) {
         if (a > b) {
             return a;
@@ -16,16 +16,23 @@ public class Ejercicio4 {
         return mayor(maxParcial, c);
     }
 
+    static int mayor(int a, int b, int c, int d) {
+        int maxParcial2 = mayor(a, b, c);
+        return mayor(maxParcial2, d);
+    }
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        int x, y, z;
+        int x, y, z, v;
         System.out.print("Primer valor: ");
         x = lector.nextInt();
         System.out.print("Segundo valor: ");
         y = lector.nextInt();
         System.out.print("Tercer valor: ");
         z = lector.nextInt();
+        System.out.print("Cuatro valor: ");
+        v = lector.nextInt();
 
-        System.out.println("El mayor valor es " + mayor(x, y, z));
+        System.out.println("El mayor valor es " + mayor(x, y, z, v));
     }
 }
---
+++ Programacion/Unidad_1/Ejercicios_1_3/Ejercicio6.java
@@ -0,0 +1,5 @@
+package Programacion.Unidad_1.Ejercicios_1_3;
+
+public class Ejercicio6 {
+    
+}
