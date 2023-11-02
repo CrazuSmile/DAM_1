9ce49fb69f814b31de1f1c65d5d90228db0d0591
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-11-02 09:44:19 +0000

    new file:   Programacion/Unidad_1/Ejercicios_1_3/Ejercicio9.java

---
+++ Programacion/Unidad_1/Ejercicios_1_3/Ejercicio9.java
@@ -0,0 +1,13 @@
+package Programacion.Unidad_1.Ejercicios_1_3;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    static double mediaArmonica(double num1, double num2) {
+        return ((num1 + num2) / 2) / ((1 / num1) + (1 / num2));
+    }
+    public static void main(String[] args) {
+        int x, y;
+        Scanner lector = new Scanner(System.in);
+    }
+}
