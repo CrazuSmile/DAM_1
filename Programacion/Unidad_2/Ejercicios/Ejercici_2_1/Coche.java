34f06e8c8ba6738e8c71d489d45849985d451713
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-11-09 08:17:09 +0000

    new file:   Programacion/Unidad_2/Ejercicios/Ejercici_2_1/Coche.java 	new file:   Programacion/Unidad_2/Ejercicios/Ejercici_2_1/imprimirCoche.java

---
+++ Programacion/Unidad_2/Ejercicios/Ejercici_2_1/Coche.java
@@ -0,0 +1,22 @@
+package Programacion.Unidad_2.Ejercicios.Ejercici_2_1;
+
+public class Coche {
+    private String modelo;
+    private String color;
+    private String matricula;
+    private String tipoCoche;
+    private boolean pintura;
+    private int año;
+
+    public void imprimeCoche(String nomModelo, String nomColor, boolean metalico) {
+        modelo = nomModelo;
+        color = nomColor;
+        pintura = metalico;
+
+        if (metalico) {
+            System.out.print(modelo + " " + color + " metalico");
+        } else {
+            System.out.print(modelo + " " + color + " no metalico");
+        }
+    }
+}
