6969ea734ca82b444af43b356ce488172d2f64b5
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-11-13 12:04:19 +0000

    new file:   Programacion/Unidad_2/Ejercicios/Ejercicio_2_1/Ejercicio5/Movil.java

---
+++ Programacion/Unidad_2/Ejercicios/Ejercicio_2_1/Ejercicio5/Movil.java
@@ -0,0 +1,17 @@
+package Programacion.Unidad_2.Ejercicios.Ejercicio_2_1.Ejercicio5;
+
+public class Movil {
+    private double x, y, velocidad, tiempo;
+
+    Movil(double x, double y, double velocidad, double tiempo) {
+        this.x = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        this.tiempo = tiempo;
+    }
+
+    public void movimiento(){
+        y = 2*x + 1;
+    }
+
+}
