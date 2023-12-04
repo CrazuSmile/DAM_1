46b6df720ef41ec66a80d72a9a6f420bcce6553a
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-12-04 10:36:36 +0000

    modified:   Programacion/Unidad_3/teoria/ejemplo1/SchoolApp.java

--- Programacion/Unidad_3/teoria/ejemplo1/SchoolApp.java
+++ Programacion/Unidad_3/teoria/ejemplo1/SchoolApp.java
@@ -3,14 +3,12 @@ package Programacion.Unidad_3.teoria.ejemplo1;
 public class SchoolApp {
     public static void main(String[] args) {
         Alumno alumno1 = new Alumno();
-        Persona persona1 = new Persona();
-        Horario horario1 = new Horario();
 
-        persona1.nombre = "Juan Jose";
-        persona1.apellidos = "Sanz Lopez";
-        persona1.añoDeNacimiento = 2000;
+        alumno1.nombre = "Juan Jose";
+        alumno1.apellidos = "Sanz Lopez";
+        alumno1.añoDeNacimiento = 2000;
 
-        persona1.imprime();
+        alumno1.imprime();
 
         alumno1.ponGrupo("Dog", null);
     }
