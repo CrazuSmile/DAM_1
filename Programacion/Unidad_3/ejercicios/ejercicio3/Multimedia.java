fcff3122524af13cc01feedb94056937ee599fa9
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2023-12-18 11:01:05 +0000

    modified:   Programacion/Unidad_3/ejercicios/ejercicio3/Multimedia.java

--- Programacion/Unidad_3/ejercicios/ejercicio3/Multimedia.java
+++ Programacion/Unidad_3/ejercicios/ejercicio3/Multimedia.java
@@ -18,15 +18,84 @@ public class Multimedia {
         this.autor = autor;
         this.añoPublicacion = añoPublicacion;
         this.miFormato = miFormato;
-        this.hora = hora;
-        this.minuto = minuto;
-        this.segundo = segundo;
+        if (hora >= 0) {
+            this.hora = hora;
+        }
+        if (minuto > 0 && minuto < 60) {
+            this.minuto = minuto;
+        }
+        if (segundo > 0 && segundo < 60) {
+            this.segundo = segundo;
+        }
+    }
 
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAñoPublicacion() {
+        return añoPublicacion;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public Formato getMiFormato() {
+        return miFormato;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setAñoPublicacion(int añoPublicacion) {
+        this.añoPublicacion = añoPublicacion;
+    }
+
+    public void setHora(int hora) {
+        if (hora >= 0) {
+            this.hora = hora;
+        }
+    }
+
+    public void setMiFormato(Formato miFormato) {
+        this.miFormato = miFormato;
+    }
+
+    public void setMinuto(int minuto) {
+        if (minuto > 0 && minuto < 60) {
+            this.minuto = minuto;
+
+        }
+    }
+
+    public void setSegundo(int segundo) {
+        if (segundo > 0 && segundo < 60) {
+            this.segundo = segundo;
+        }
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     @Override
     public String toString() {
         // TODO Auto-generated method stub
-        return titulo+"."+miFormato+", "+autor+", " +", "+añoPublicacion+", "+ hora + ":" + minuto + ":" + segundo;
+        return titulo + "." + miFormato + ", " + autor + ", " + ", " + añoPublicacion + ", " + hora + ":" + minuto + ":"
+                + segundo;
     }
 }
