8e6bc8b3926b00cf464225e9e6935b4859d4fbaf
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2024-01-18 09:32:19 +0000

    new file:   Programacion/Unidad_4/ejercicio1/ejercicio4/App.java 	new file:   Programacion/Unidad_4/ejercicio1/ejercicio4/Asegurado.java 	new file:   Programacion/Unidad_4/ejercicio1/ejercicio4/Seguro.java 	new file:   Programacion/Unidad_4/ejercicio1/ejercicio4/SeguroDeceso.java 	new file:   Programacion/Unidad_4/ejercicio1/ejercicio4/SeguroVida.java

---
+++ Programacion/Unidad_4/ejercicio1/ejercicio4/SeguroVida.java
@@ -0,0 +1,28 @@
+package Programacion.Unidad_4.ejercicio1.ejercicio2;
+
+public class SeguroVida extends Seguro {
+    @Override
+    public float calcularPrimaAnual() {
+        // TODO Auto-generated method stub
+        if (asegurado.edad() <= 25) {
+            return asegurado.edad() * 1;
+        }
+        if (asegurado.edad() >= 26 && asegurado.edad() <= 34) {
+            return (float) (asegurado.edad() * 1.1);
+        }
+        if (asegurado.edad() >= 35 && asegurado.edad() <= 44) {
+            return (float) (asegurado.edad() * 1.2);
+        }
+        if (asegurado.edad() >= 45 && asegurado.edad() <= 54) {
+            return (float) (asegurado.edad() * 1.3);
+        }
+        if (asegurado.edad() >= 55 && asegurado.edad() <= 64) {
+            return (float) (asegurado.edad() * 1.4);
+        }
+        else {
+            System.out.println("No se puede contrata seguro de vida.");
+            return 0;
+        }
+
+    }
+}
