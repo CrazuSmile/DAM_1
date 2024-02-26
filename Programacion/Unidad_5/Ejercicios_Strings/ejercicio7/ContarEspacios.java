1afddb9c396b8190eb0a5023614fc124a2ca54db
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2024-02-26 11:36:52 +0000

    new file:   Programacion/Unidad_5/Ejercicios_Strings/ejercicio7/ContarEspacios.java

---
+++ Programacion/Unidad_5/Ejercicios_Strings/ejercicio7/ContarEspacios.java
@@ -0,0 +1,17 @@
+package Programacion.Unidad_5.Ejercicios_Strings.ejercicio7;
+
+import java.util.Scanner;
+
+public class ContarEspacios {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String frase;
+
+        System.out.print("Introduza uan frase: ");
+        frase = scn.nextLine();
+        int primerEspacio = frase.indexOf(" ");
+
+
+
+    }
+}
