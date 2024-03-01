867ab0f7e41992f05b1c47835fd06550b4b662db
CrazuSmile <114513354+CrazuSmile@users.noreply.github.com>
2024-03-01 12:13:32 +0000

    modified:   Programacion/Unidad_4/ejercicio1/ejercicio2/Asegurado.java 	modified:   Programacion/Unidad_4/ejercicio1/ejercicio2/Seguro.java 	new file:   Programacion/Unidad_4/ejercicio1/ejercicio2/SeguroDecesos.java 	modified:   Programacion/Unidad_4/ejercicio1/ejercicio2/SeguroVida.java 	new file:   Programacion/Unidad_4/ejercicio1/ejercicio2/TestApp.java

--- Programacion/Unidad_4/ejercicio1/ejercicio2/SeguroVida.java
+++ Programacion/Unidad_4/ejercicio1/ejercicio2/SeguroVida.java
@@ -1,28 +1,65 @@
-package Programacion.Unidad_4.ejercicio1.ejercicio2;
-
-public class SeguroVida extends Seguro {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2;
+/**
+ * La prima de un seguro de vida se calcula como un factor multiplicado por la edad del asegurado. 
+ * <p>
+ * La correlación del factor, a su vez, se calcula en base a la siguiente relación: 
+ * @author jsanm
+ */
+public class SeguroVida extends Seguro{
+    
+    /**
+     * Crea un seguro de decesos informando el asegurado. Delega en el constructor heredado ya que
+     * no se aportan nuevas propiedades en esta clase.
+     * @param asegurado Asegurado del seguro en construcción
+     * @see Seguro#Seguro(ejercicio2.Asegurado) 
+     */
+    public SeguroVida(Asegurado asegurado){
+        super(asegurado);
+    }
+    
+    
+    /**
+     * Calcula la prima anual en base a la edad del asegurado y un factor de correlación para multiplicarlos
+     * @return La prima anual
+     * @see Seguro#calcularPrimaAnual() 
+     * @see Asegurado#calcularEdad() 
+     */
     @Override
     public float calcularPrimaAnual() {
-        // TODO Auto-generated method stub
-        if (asegurado.edad() <= 25) {
-            return asegurado.edad() * 1;
-        }
-        if (asegurado.edad() >= 26 && asegurado.edad() <= 34) {
-            return (float) (asegurado.edad() * 1.1);
-        }
-        if (asegurado.edad() >= 35 && asegurado.edad() <= 44) {
-            return (float) (asegurado.edad() * 1.2);
-        }
-        if (asegurado.edad() >= 45 && asegurado.edad() <= 54) {
-            return (float) (asegurado.edad() * 1.3);
-        }
-        if (asegurado.edad() >= 55 && asegurado.edad() <= 64) {
-            return (float) (asegurado.edad() * 1.4);
-        }
+        
+        int edadAsegurado = this.asegurado.calcularEdad();
+        
+        float factorCorrelacion = calcularFactorCorrelacion(edadAsegurado);
+        
+        return factorCorrelacion * edadAsegurado;
+    }
+
+    /**
+     * Calcula un factor de correlación multiplicativo de la edad en base a un algoritmo 
+     * definido por el usuario.
+     * @param edad edad cuyo factor se desea calcular. Máximo de 64 años, en caso contrario genera un error.
+     * @return El factor
+     */
+    private float calcularFactorCorrelacion(int edad) {
+        if (edad < 25 )
+            return 1;
+        else if (edad >= 25 && edad <= 34)
+            return 1.1f;
+        else if (edad >= 35 && edad <= 44)
+            return 1.2f;
+        else if (edad >= 45 && edad <= 54)
+            return 1.3f;
+        else if (edad >= 55 && edad <= 64)
+            return 1.4f;
         else {
-            System.out.println("No se puede contrata seguro de vida.");
+            //throw new RuntimeException("Edad no válida de contratación para un seguro de vida");
+            System.out.println("Edad no válida de contratación para un seguro de vida");
             return 0;
         }
-
     }
+    
 }
